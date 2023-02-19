c4c2ac37d1645e7338e58a871b6ecd8dfa582327
Gabriel Badaró <grbadas@gmail.com>
2023-02-18 21:12:53 -0300

    update

---
+++ src/test/java/com/badas/todo/SpringbootTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badas.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
